6a2b7064ceb116f21948a5cf0230040b9543d926
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-10-13 12:12:09 +0200

    post request

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import express from 'express'; // const express = require('express'); package.js
 
 const app = express();
 
+app.use(express.json()); // allows to read json in req
+
 // req -> what client send to me(for exp. from frontend) && res -> what i will send to client
 app.get("/", (req, res) => {
-    res.send("hello world"); 
+    res.send("hello world!!!");
 });
 
-app.listen(5432, (err) => {
+app.post("/auth/login", (req, res) => {
+    console.log(req.body); // req.body -> json from post request
+    
+    res.json({
+        success: true
+    });
+});
+
+app.listen(4500, (err) => {
     if (err) {
         console.log(err);
     } else {
