94e131a631c9d559360c4fedb985078627c7cdbd
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2022-11-25 16:41:43 +0100

    cleaned the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./scss/_app.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <h1>I am going to try formik in this small proj</h1>
+      <p>Email input</p>
+      <p>Password input</p>
+      <button>Sign in</button>
+      <p>Do not have an account? Sign up</p>
+    </>
+  );
 }
 
-export default App
+export default App;
