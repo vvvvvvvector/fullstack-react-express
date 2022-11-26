5dd2e899f2092ee99656858b643f0dbd7e169a8c
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2022-11-26 02:34:52 +0100

    added checkbox with label and password and email validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,49 @@
 import React from "react";
 
-import { Formik, Form } from "formik";
+import { Formik, Form, useField, FieldAttributes } from "formik";
 
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, FormControlLabel, Checkbox } from "@mui/material";
 
 import "./scss/_app.scss";
+
+type MyTextFieldType = { type: string; label: string } & FieldAttributes<{}>;
+
+const MyTextField: React.FC<MyTextFieldType> = ({ type, label, ...props }) => {
+  const [field, meta] = useField(props);
+
+  const errorText = meta.error && meta.touched ? meta.error : "";
+
+  // {...field} -> name, onChange, onBlur properites
+  return (
+    <TextField
+      className="input"
+      {...field}
+      type={type}
+      label={label}
+      helperText={errorText}
+      error={errorText !== ""}
+    />
+  );
+};
 
 const App: React.FC = () => {
   return (
     <div className="wrapper">
       <Formik
-        initialValues={{ email: "", password: "" }}
+        initialValues={{ email: "", password: "", isCool: false }}
+        validate={(values) => {
+          const errors: Record<string, string> = {};
+
+          if (!values.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+            errors.email = "Invalid email!";
+          }
+
+          if (values.password.length < 5) {
+            errors.password = "Weak password!";
+          }
+
+          return errors;
+        }}
         onSubmit={(data, { setSubmitting }) => {
           setSubmitting(true);
 
@@ -22,25 +55,19 @@ const App: React.FC = () => {
           }, 500);
         }}
       >
-        {({ values, isSubmitting, handleChange, handleBlur }) => (
+        {({ values, errors, isSubmitting, handleChange }) => (
           <Form>
-            <TextField
-              className="input"
-              name="email"
-              type="email"
-              label="Your email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <TextField
-              className="input"
+            <MyTextField name="email" type="text" label="Your email" />
+            <MyTextField
               name="password"
               type="password"
               label="Your password"
-              value={values.password}
+            />
+            <FormControlLabel
+              name="isCool"
+              control={<Checkbox checked={values.isCool} />}
+              label="Do you agree that Formik is cool?"
               onChange={handleChange}
-              onBlur={handleBlur}
             />
             <Button
               className="submit"
@@ -51,6 +78,7 @@ const App: React.FC = () => {
               Submit
             </Button>
             <pre>{JSON.stringify(values, null, 2)}</pre>
+            <pre>{JSON.stringify(errors, null, 2)}</pre>
           </Form>
         )}
       </Formik>
