fa35c4ad7961eccaee3faf43888116ff50a12520
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2022-11-29 22:56:45 +0100

    now user can add tags for his posts

--- src/components/NewPost/NewPost.tsx
+++ src/components/NewPost/NewPost.tsx
@@ -3,20 +3,41 @@ import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
 
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Chip } from "@mui/material";
 
 import { Formik, Form } from "formik";
 
 import styles from "./NewPost.module.scss";
 
+interface TagType {
+  key: number;
+  value: string;
+}
+
 export const NewPost: React.FC = () => {
   const navigate = useNavigate();
+
+  const [tagsInput, setTagsInput] = React.useState("");
+  const [tags, setTags] = React.useState<TagType[]>([]);
+
+  const handleDeleteTag = (tagToDelete: TagType) => () => {
+    setTags((tags) => tags.filter((tag) => tag.key !== tagToDelete.key));
+  };
 
   return (
     <div className={styles["form-wrapper"]}>
       <Formik
-        initialValues={{ title: "", text: "", tags: [] }}
+        initialValues={{
+          title: "",
+          text: "",
+        }}
         onSubmit={(data) => {
+          const resTags = [];
+
+          for (let i = 0; i < tags.length; i++) {
+            resTags.push(tags[i].value);
+          }
+
           if (data.text !== "" && data.title !== "") {
             axios
               .post(
@@ -24,7 +45,7 @@ export const NewPost: React.FC = () => {
                 {
                   title: data.title,
                   text: data.text,
-                  tags: data.tags,
+                  tags: resTags,
                 },
                 {
                   headers: {
@@ -62,6 +83,45 @@ export const NewPost: React.FC = () => {
               label="Text"
               multiline
             />
+            {tags.length > 0 && (
+              <div className={styles.stack}>
+                {tags.map((tag, index) => (
+                  <Chip
+                    key={index}
+                    size="small"
+                    label={tag.value}
+                    onDelete={handleDeleteTag(tag)}
+                  />
+                ))}
+              </div>
+            )}
+            <div className={styles["tags-input"]}>
+              <TextField
+                sx={{
+                  width: "70%",
+                }}
+                onChange={(e) => {
+                  setTagsInput(e.target.value);
+                }}
+                value={tagsInput}
+                label="Tag"
+                variant="filled"
+              />
+              <Button
+                onClick={() => {
+                  if (tagsInput !== "") {
+                    setTags((prev) => [
+                      ...prev,
+                      { key: tags.length, value: tagsInput },
+                    ]);
+                    setTagsInput("");
+                  }
+                }}
+                variant="outlined"
+              >
+                Add tag
+              </Button>
+            </div>
             <Button
               sx={{
                 position: "relative",
